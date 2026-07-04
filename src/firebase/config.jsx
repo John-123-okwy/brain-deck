a37b6319ea1059c17e7fa67e8a852a306495b384
John-123-okwy <johnchukwu511@gmail.com>
2026-07-04 13:29:34 +0100

    updated app

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -8,23 +8,23 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+const firebaseConfig = {/*
      apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId:import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,*/
   
-  /*
+  
   apiKey: "AIzaSyBl9eUm1CL53GFgWcMMAhf1afGqkJ0OtUk",
   authDomain: "cj-quizapp.firebaseapp.com",
   databaseURL: "https://cj-quizapp-default-rtdb.firebaseio.com",
   projectId: "cj-quizapp",
   storageBucket: "cj-quizapp.firebasestorage.app",
   messagingSenderId: "909622350944",
-  appId: "1:909622350944:web:978f24cbe0901977ca9f05"*/
+  appId: "1:909622350944:web:978f24cbe0901977ca9f05"
 };
 
 // Initialize Firebase
